0de1c10bf69ad84f75bdc33a631d3f8a8c915e01
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2022-05-24 02:29:15 +0600

    Update index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
 
+const fs = require('fs'), { readdir, mkdir, rm } = require('fs/promises'),  path = require('path'), Folder = path.join(__dirname, 'files'), newFolder = path.join(__dirname, 'files-copy');
+
+
+const copyDirection = async (folder, newFolder) => {
+  await rm(newFolder, { recursive: true, force: true });
+  fs.access(newFolder, err => {
+    if(err){
+      mkdir(newFolder, { recursive: true })
+    } 
+  })
+  await readdir(folder).then(files => files.forEach(file => {
+    fs.copyFile(`${folder}/${file}`, `${newFolder}/${file}`,(err) => {
+        if (err) {
+          console.log("Error Found:", err);
+        }
+      });
+  }))
+};
+
+copyDirection(Folder, newFolder);
